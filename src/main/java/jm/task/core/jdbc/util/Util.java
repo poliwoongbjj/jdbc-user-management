23d8006248a8d897ef2dde4a668280d382f5d70c
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-06-02 11:49:43 +0900

    Add Util.java without credentials

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // set up a database connection
+    // Database connection parameters
+    private static final String URL = "jdbc:mysql://localhost:3306/database_name";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "<PASSWORD>";
+
+    // JDBC driver
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    // Method to get database connection
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Load the driver
+            Class.forName(DRIVER);
+
+            // Establish connection
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Connection to database failed!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
