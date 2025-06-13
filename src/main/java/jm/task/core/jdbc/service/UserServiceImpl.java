8904fb4ac7706a7c55f4d6ad87bfaef01cd24b70
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-06-13 18:59:15 +0900

    Update UserServiceImpl to Use Hibernate DAO

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,14 +1,14 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    // Service reuses methods from DAO
-    private final UserDao userDao = new UserDaoJDBCImpl();
+    // Change from JDBC to Hibernate implementation
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
