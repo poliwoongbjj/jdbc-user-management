3f326ce5e507530eab4f5bd522f5b9c9c051b608
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-06-13 18:50:29 +0900

    Update User.java with Complete Hibernate Annotations

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,25 +1,26 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    @Column
+    @Column(name = "name", nullable = false, length = 45)
     private String name;
 
-    @Column
+    @Column(name = "lastName", nullable = false, length = 45)
     private String lastName;
 
-    @Column
+    @Column(name = "age", nullable = false)
     private Byte age;
 
     public User() {
-
+        // Default constructor required by Hibernate
     }
 
     public User(String name, String lastName, Byte age) {
