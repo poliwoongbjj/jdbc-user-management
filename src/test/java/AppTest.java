7bc9aebf75b606f3efdc31fbb4b8fcc4ebe34927
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-06-21 01:13:37 +0900

    Initial commit: Bean Wiring in Spring

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,30 +1,32 @@
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AppConfig.class)
-public class AppTest {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Test
-    public void mainTest() {
-        HelloWorld tree =
-                applicationContext.getBean(HelloWorld.class);
-        HelloWorld leaf =
-                applicationContext.getBean(HelloWorld.class);
-
-        Cat one = applicationContext.getBean(Cat.class);
-        Cat two = applicationContext.getBean(Cat.class);
-
-        Assert.assertSame("Test failed, incorrect HelloWorld bean configuration", tree, leaf);
-        Assert.assertNotSame("Test failed, incorrect Cat bin configuration", one, two);
-    }
-}
+//import app.config.AppConfig;
+//import app.model.AnimalsCage;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = AppConfig.class)
+//public class MainTest {
+//
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @Test
+//    public void mainTest() {
+//        long time = 0;
+//        for (int i = 0; i < 5; i++) {
+//            AnimalsCage bean =
+//                    applicationContext.getBean(AnimalsCage.class);
+//            if (i == 0) {
+//                time = bean.getTimer().getTime();
+//                continue;
+//            }
+//            Assert.assertEquals("Test failed, incorrect bean implementation.", time, bean.getTimer().getTime().longValue());
+//            System.out.println(time);
+//        }
+//    }
+//}
