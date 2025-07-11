c5ae9c1fdeb184219aab05a4ef9f93eaf3d33d7f
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-07-11 16:54:56 +0900

    Enhance DAO layer with new method and implement HQL query in UserDaoImp

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,13 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession()
+              .createQuery("from User u where u.car.model = :model and u.car.series = :series");
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getSingleResult();
+   }
+
 }
