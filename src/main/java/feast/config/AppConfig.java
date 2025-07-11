6b4a035a1336160aa287a9a6762606a2524cffdc
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-07-11 11:31:44 +0900

    Add @Bean method to create Camel8 instance

--- src/main/java/feast/config/AppConfig.java
+++ src/main/java/feast/config/AppConfig.java
@@ -1,5 +1,6 @@
 package feast.config;
 
+import feast.models.Camel8;
 import feast.models.Chicken3;
 import feast.models.Duck2;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,10 @@ public class AppConfig {
     @Bean
     public static Duck2 getDuck(Chicken3 chicken) {
         return new Duck2(chicken);
+    }
+
+    @Bean
+    public static Camel8 getCamel() {
+        return new Camel8();
     }
 }
