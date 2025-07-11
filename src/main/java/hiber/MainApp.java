ee95f96c045283681323acea801b12bc8ee62900
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-07-11 19:12:27 +0900

    Update MainApp to test the new functionality

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -2,6 +2,7 @@ package hiber;
 
 import hiber.config.AppConfig;
 import hiber.model.User;
+import hiber.model.Car;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,18 +16,37 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("BMW", 5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Audi", 6)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Mercedes", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Toyota", 7)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         if (user.getCar() != null) {
+            System.out.println("Car = " + user.getCar().getModel() + " " + user.getCar().getSeries());
+         }
          System.out.println();
+      }
+
+      // Test the HQL query method
+      System.out.println("=== Testing HQL Query ===");
+      try {
+         User userWithBMW = userService.getUserByCarModelAndSeries("BMW", 5);
+         System.out.println("User with BMW 5 series: " + userWithBMW.getFirstName() + " " + userWithBMW.getLastName());
+      } catch (Exception e) {
+         System.out.println("No user found with BMW 5 series");
+      }
+
+      try {
+         User userWithAudi = userService.getUserByCarModelAndSeries("Audi", 6);
+         System.out.println("User with Audi 6 series: " + userWithAudi.getFirstName() + " " + userWithAudi.getLastName());
+      } catch (Exception e) {
+         System.out.println("No user found with Audi 6 series");
       }
 
       context.close();
